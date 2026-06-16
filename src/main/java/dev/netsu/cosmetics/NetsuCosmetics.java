9015a0df80496826b5d7c8bf8144d176d7e5f2a4
AstrenDev <galleta1534oficial@gmail.com>
2026-06-16 10:19:27 +0200

    Update NetsuCosmetics.java

--- src/main/java/dev/netsu/cosmetics/NetsuCosmetics.java
+++ src/main/java/dev/netsu/cosmetics/NetsuCosmetics.java
@@ -1,5 +1,5 @@
 package dev.netsu.cosmetics;
-
+ 
 import dev.netsu.cosmetics.command.NcCommand;
 import dev.netsu.cosmetics.command.NcTabCompleter;
 import dev.netsu.cosmetics.cosmetic.CosmeticData;
@@ -12,6 +12,7 @@ import dev.netsu.cosmetics.listener.BrisaMarinaListener;
 import dev.netsu.cosmetics.listener.AuraVeraniegarListener;
 import dev.netsu.cosmetics.listener.CosmeticApplyListener;
 import dev.netsu.cosmetics.listener.CaminoSoleadoListener;
+import dev.netsu.cosmetics.listener.EstelarSolarListener;
 import dev.netsu.cosmetics.util.ColorUtil;
 import dev.netsu.cosmetics.util.WorldGuardHelper;
 import net.kyori.adventure.text.Component;
@@ -19,28 +20,28 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
-
+ 
 import java.util.ArrayList;
 import java.util.List;
-
+ 
 public class NetsuCosmetics extends JavaPlugin {
-
+ 
     private static NetsuCosmetics instance;
     private CosmeticManager cosmeticManager;
     private DataManager dataManager;
     private MessagesManager messagesManager;
-
+ 
     @Override
     public void onEnable() {
         instance = this;
         saveDefaultConfig();
         ConfigUpdater.update(this);
         WorldGuardHelper.init();
-
+ 
         dataManager = new DataManager(this);
         messagesManager = new MessagesManager(this);
         cosmeticManager = new CosmeticManager(this);
-
+ 
         getCommand("nc").setExecutor(new NcCommand(this));
         getCommand("nc").setTabCompleter(new NcTabCompleter(this));
         getServer().getPluginManager().registerEvents(new CosmeticApplyListener(this), this);
@@ -48,13 +49,14 @@ public class NetsuCosmetics extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new AuraVeraniegarListener(this), this);
         getServer().getPluginManager().registerEvents(new BrisaMarinaListener(this), this);
         getServer().getPluginManager().registerEvents(new GroundLandListener(this), this);
+        getServer().getPluginManager().registerEvents(new EstelarSolarListener(this), this);
     }
-
+ 
     @Override
     public void onDisable() {
         if (dataManager != null) dataManager.save();
     }
-
+ 
     public void reload() {
         ConfigUpdater.update(this);
         dataManager.reload();
@@ -62,7 +64,7 @@ public class NetsuCosmetics extends JavaPlugin {
         cosmeticManager.reload();
         updateOnlinePlayersItems();
     }
-
+ 
     private void updateOnlinePlayersItems() {
         for (Player player : getServer().getOnlinePlayers()) {
             for (ItemStack item : player.getInventory().getContents()) {
@@ -71,21 +73,21 @@ public class NetsuCosmetics extends JavaPlugin {
             player.updateInventory();
         }
     }
-
+ 
     private void updateItemLores(ItemStack item) {
         if (item == null || item.getType().isAir() || !item.hasItemMeta()) return;
         ItemMeta meta = item.getItemMeta();
         if (!meta.hasLore()) return;
         List<Component> currentLore = meta.lore();
         if (currentLore == null) return;
-
+ 
         for (CosmeticData data : cosmeticManager.getAll()) {
             boolean found = false;
             for (Component line : currentLore) {
                 if (line != null && ColorUtil.plain(line).contains(data.nombreDisplay)) { found = true; break; }
             }
             if (!found) continue;
-
+ 
             List<Component> newLore = new ArrayList<>();
             int i = 0;
             while (i < currentLore.size()) {
@@ -110,7 +112,7 @@ public class NetsuCosmetics extends JavaPlugin {
         }
         item.setItemMeta(meta);
     }
-
+ 
     public static NetsuCosmetics getInstance() { return instance; }
     public CosmeticManager getCosmeticManager() { return cosmeticManager; }
     public DataManager getDataManager() { return dataManager; }
